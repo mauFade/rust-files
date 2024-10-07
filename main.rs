2131854f1394ef4adaeef1d9b607dfe3983e6271
Mauricio Cardoso <mauricio.cds00@gmail.com>
2024-10-07 16:25:42 -0300

    feat: read file and print

--- main.rs
+++ main.rs
@@ -1,5 +1,13 @@
-use ::std::fs::File;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    println!("HELLO")
+    let mut file = File::open("file.txt").expect("Error reading file from fs");
+
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)
+        .expect("Error when serialazing file to string");
+
+    println!("File content:\n\n{}", content);
 }
